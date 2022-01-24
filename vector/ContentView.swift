063725fb248ec0f8b1b9ecd3ff2bbcfb9c173f1e
Juhannuspukki <tepzis-Buhteg-xiwbe7>
2022-01-24 17:33:32 +0200

    Initial Commit

---
+++ vector/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  vector
+//
+//  Created by Jere Laine on 24.1.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ vector/vectorApp.swift
@@ -0,0 +1,17 @@
+//
+//  vectorApp.swift
+//  vector
+//
+//  Created by Jere Laine on 24.1.2022.
+//
+
+import SwiftUI
+
+@main
+struct vectorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
